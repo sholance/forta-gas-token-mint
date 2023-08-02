2e71b4848f4519c5938fc73d431b669cc7fcfe4e
xhola <ohksamie@gmail.com>
2023-08-02 16:28:40 +0100

    updated to parse popular functions

--- src/agent.ts
+++ src/agent.ts
@@ -19,31 +19,74 @@ export const initialize = (provider: providers.Provider) => {
 // rolling average over 5000 transactions
 const sdMathVar = new SdMath(5000);
 
+
 function provideHandleTransaction(rollingMath: { getAverage: () => any; getStandardDeviation: () => any; addElement: (arg0: any) => void; },
   networkData: NetworkData,
 ) {
   return async function handleTransaction(txEvent: TransactionEvent) {
     const findings: Finding[] = [];
+
+
+      const frequency: { [key: string]: bigint } = {};
+
+      const functionHash = txEvent.transaction.data.slice(0, 10);
+      if (functionHash in frequency) {
+          frequency[functionHash]++;
+      } else {
+          frequency[functionHash] = BigInt(1);
+      }
+
+      // Filter out the most popular functionHashes
+      const popularFunctionHasheslist = Object.keys(frequency).filter((functionHash) => {
+          const mostPopularFunctionHashes = [
+              "0x095ea7b3", // name
+              "0x313ce567", // symbol
+              "0x18160ddd", // totalSupply
+              "0x70a08231", // balanceOf
+              "0xa9059cbb", // transfer
+              "0x23b872dd", // approve
+              "0x8f20c609", // allowance
+              "0x42966c68", // transferFrom
+              "0x4e71d92d", // increaseAllowance
+              "0x5b5e139f", // decreaseAllowance
+              "0x095ea7b3", // name
+              "0x313ce567", // symbol
+              "0x8c5be1e5", // decimals
+          ];
+          return mostPopularFunctionHashes.includes(functionHash);
+      });
+
+
+
+
+      for (const popularFunctionHash of popularFunctionHasheslist) {
+          console.log(`${popularFunctionHash} is here`)
+      //   const gasCost = mintGasUsed.multipliedBy(txEvent.gasPrice);
+      //random test
+      //   if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
+
+
       const ercFunctions = txEvent.filterLog([
           BEP20_APPROVE_FUNCTION_SIG,
           BEP20_INCREASE_ALLOWANCE_FUNCTION_SIG,
       ]);
 
-
       for (const ercFunction of ercFunctions) {
           const gasUsed = new BigNumber((await getTransactionReceipt(txEvent.hash)).gasUsed);
+
         let functionGasUsed = gasUsed;
           const average = rollingMath.getAverage();
           const standardDeviation = rollingMath.getStandardDeviation();
+
           //   const gasCost = mintGasUsed.multipliedBy(txEvent.gasPrice);
-
           //random test
-          if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
-              console.log(`${functionGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
-          } else {
-              console.log(`${functionGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
-          }
+          //   if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
+          //       console.log(`${functionGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
+          //   } else {
+          //       console.log(`${functionGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
+          //   }
 
+          console.log(ercFunction.args)
           // create finding if gas price is over 2 times standard deviations above the past 5000 txs
           if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
               findings.push(
@@ -89,12 +132,13 @@ function provideHandleTransaction(rollingMath: { getAverage: () => any; getStand
           const average = rollingMath.getAverage();
           const standardDeviation = rollingMath.getStandardDeviation();
           // create finding if gas price is over 2 times standard deviations above the past 5000 txs
-          //test
-          if (mintGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
-              console.log(`${mintGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
-          } else {
-              console.log(`${mintGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
-          }
+          //devaition test
+          //   if (mintGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
+          //       console.log(`${mintGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
+          //   } else {
+          //       console.log(`${mintGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
+          //   }
+
           if (mintGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
               try {
                   findings.push(
@@ -127,6 +171,7 @@ function provideHandleTransaction(rollingMath: { getAverage: () => any; getStand
           rollingMath.addElement(mintGasUsed);
           // rolling average updated
       })
+      }
       return findings;
   };
 }
