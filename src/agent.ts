4cf8b689c0011fc53f6d1b892e7509595acd3fa8
xhola <ohksamie@gmail.com>
2023-08-02 21:16:17 +0100

    update stdev to 3

--- src/agent.ts
+++ src/agent.ts
@@ -8,6 +8,8 @@ import { BEP20_APPROVE_FUNCTION_SIG, BEP20_INCREASE_ALLOWANCE_FUNCTION_SIG, TRAN
 
 
 const networkManager = new NetworkManager(NETWORK_MAP);
+let findingsCount = 0;
+
 
 export const initialize = (provider: providers.Provider) => {
     return async () => {
@@ -24,6 +26,9 @@ function provideHandleTransaction(rollingMath: { getAverage: () => any; getStand
 ) {
   return async function handleTransaction(txEvent: TransactionEvent) {
     const findings: Finding[] = [];
+
+      // limiting this agent to emit only 5 findings so that the alert feed is not spammed
+      if (findingsCount >= 5) return findings;
 
 
       const frequency: { [key: string]: bigint } = {};
@@ -69,29 +74,29 @@ function provideHandleTransaction(rollingMath: { getAverage: () => any; getStand
 
           //   const gasCost = mintGasUsed.multipliedBy(txEvent.gasPrice);
           //random test
-          //   if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
-          //       console.log(`${functionGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
+          //   if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(3)))) {
+          //       console.log(`${functionGasUsed} is bigger than ${average.plus(standardDeviation.times(3))}`)
           //   } else {
-          //       console.log(`${functionGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
+          //       console.log(`${functionGasUsed} is not bigger than ${average.plus(standardDeviation.times(3))}`)
           //   }
 
-          // create finding if gas price is over 2 times standard deviations above the past 5000 txs
-          if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
+          // create finding if gas price is over 3 times standard deviations above the past 5000 txs
+          if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(3)))) {
               findings.push(
                   Finding.fromObject({
                       name: "Suspected high gas token mint",
                       description: `Suspicious approval with gas  detected: ${functionGasUsed}`,
-                      alertId: "GAS-ANOMALOUS-LARGE-APPROVAL",
+                      alertId: "GAS-ANOMALOUS-LARGE-CONSUMPTION",
                       severity: FindingSeverity.High,
                       type: FindingType.Info,
                       metadata: {
                           to: JSON.stringify(txEvent.transaction.to),
-                          value: JSON.stringify(functionGasUsed)
+                          value: JSON.stringify(gasUsed)
                       },
                       labels: [{
                           entityType: EntityType.Address,
                           entity: JSON.stringify(txEvent.transaction.to),
-                          label: "sus-gas-token-mint",
+                          label: "sus-gas-consumption",
                           confidence: 0.8,
                           remove: false,
                           metadata: {
@@ -103,60 +108,6 @@ function provideHandleTransaction(rollingMath: { getAverage: () => any; getStand
           }
           rollingMath.addElement(functionGasUsed);
 
-
-      // const mintEvents = txEvent.filterLog(GAS_TOKEN_ABI, networkData.gasAddress);
-      const MintEvents = txEvent
-          .filterLog(TRANSFER_EVENT)
-          .filter((transferEvent) => {
-              const { from } = transferEvent.args;
-              return from === "0x0000000000000000000000000000000000000000";
-          });
-
-      //   const gasCost = mintGasUsed.multipliedBy(txEvent.gasPrice);
-
-      MintEvents.forEach((mintEvent) => {
-          const average = rollingMath.getAverage();
-          const standardDeviation = rollingMath.getStandardDeviation();
-          // create finding if gas price is over 2 times standard deviations above the past 5000 txs
-          //devaition test
-          //   if (mintGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
-          //       console.log(`${mintGasUsed} is bigger than ${average.plus(standardDeviation.times(2))}`)
-          //   } else {
-          //       console.log(`${mintGasUsed} is not bigger than ${average.plus(standardDeviation.times(2))}`)
-          //   }
-
-          if (functionGasUsed.isGreaterThan(average.plus(standardDeviation.times(2)))) {
-              try {
-                  findings.push(
-                      Finding.fromObject({
-                          name: "Suspicious gas token mint",
-                          description: `Suspicious mint of gas token detected: ${mintGasUsed}`,
-                          alertId: "GAS-ANOMALOUS-TOKEN-MINT",
-                          severity: FindingSeverity.High,
-                          type: FindingType.Info,
-                          metadata: {
-                              to: JSON.stringify(txEvent.transaction.to),
-                              value: `${mintGasUsed}`
-              },
-              labels: [{
-                  entityType: EntityType.Address,
-                  entity: JSON.stringify(txEvent.transaction.to),
-                  label: "sus-gas-token-mint",
-                  confidence: 0.8,
-                  remove: false,
-                  metadata: {},
-              }]
-                      }
-                      ))
-
-              }
-              catch (error) {
-                  console.log(error);
-              }
-          }
-          rollingMath.addElement(functionGasUsed);
-          // rolling average updated
-      })
       }
       return findings;
   };
